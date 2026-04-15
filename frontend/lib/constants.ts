350e1dd5d4513a750777b9690c1fc51830463aff
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-15 15:45:45 +0300

    docs: update constants notes

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -8,3 +8,4 @@ export const LEADERBOARD_SIZE = 10;
 // rep-constants-lib: 1776171495088
 // rep-constants-lib: 1776187038693
 // rep-constants-lib: 1776248495006
+// rep-constants-lib: 1776257145021
