767cbec640dba7045ba02913cffacce5727735b1
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-18 00:25:47 +0300

    perf: annotate refresh path tuning

--- frontend/lib/stacks.ts
+++ frontend/lib/stacks.ts
@@ -101,3 +101,4 @@ export async function sendGuess(value: number) {
 // rep-stacks-lib: 1776432378222
 // rep-stacks-lib-refresh: 1776432540658
 // rep-stacks-lib-wallet: 1776432584243
+// rep-stacks-lib-refresh: 1776461147079
