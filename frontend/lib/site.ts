8629692b7be50c17e296a123e02b6f98084ed72b
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-19 20:37:51 +0300

    seo: annotate site metadata helpers

--- frontend/lib/site.ts
+++ frontend/lib/site.ts
@@ -43,3 +43,4 @@ export const SITE_KEYWORDS = ["number guess", "stacks game", "prediction app", "
 // rep-site-lib: 1776550925931
 // rep-site-lib-metadata: 1776586294256
 // rep-site-lib: 1776586485959
+// rep-site-lib-metadata: 1776620271309
