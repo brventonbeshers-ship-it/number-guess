08be15874915cf6f67e780913677263a96ca3d26
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-19 11:11:10 +0300

    refactor: adjust sdk client internals

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -241,3 +241,4 @@ export class NumberGuessClient {
 // rep-sdk-client-guards: 1776550928459
 // rep-sdk-client: 1776550981137
 // rep-sdk-client-guards: 1776586261983
+// rep-sdk-client: 1776586270729
