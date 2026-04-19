33dcf3ac54f4c691790a3a549e4e4a0deec4d469
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-19 11:13:59 +0300

    docs: update polling hook markers

--- frontend/hooks/useInterval.ts
+++ frontend/hooks/useInterval.ts
@@ -31,3 +31,4 @@ export function useInterval(callback: () => void, delay: number | null) {
 // rep-hooks-interval: 1776480649469
 // rep-hooks-interval: 1776519303837
 // rep-hooks-interval: 1776551052627
+// rep-hooks-interval: 1776586439009
