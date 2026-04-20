fd9ee5397ae3522b0ec629570702f09abba6f6d7
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-20 11:22:59 +0300

    config: adjust site description annotations

--- frontend/lib/site.ts
+++ frontend/lib/site.ts
@@ -48,3 +48,4 @@ export const SITE_KEYWORDS = ["number guess", "stacks game", "prediction app", "
 // rep-site-lib: 1776645269705
 // rep-site-lib-metadata: 1776645285749
 // rep-site-lib-metadata: 1776673301216
+// rep-site-lib: 1776673379378
