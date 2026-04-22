5f99adff7553a9766d1a1f6976318f5138fd1b6d
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-22 08:25:50 +0300

    fix: improve sdk parsing guardrails

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -260,3 +260,4 @@ export class NumberGuessClient {
 // rep-sdk-client: 1776818405464
 // rep-sdk-client-guards: 1776818529848
 // rep-sdk-client-guards: 1776835301111
+// rep-sdk-client: 1776835550730
